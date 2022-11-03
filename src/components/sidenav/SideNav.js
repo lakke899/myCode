c35fc0eb645eddaa013a162c6d1b4feee991c739
lakke899 <51609315+lakke899@users.noreply.github.com>
2022-11-03 11:29:54 +0530

    Delete src directory

--- src/components/sidenav/SideNav.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './SideNav.css'
-export default function SideNav() {
-    return (
-        <div className='Navbar'>
-            <h1>SideNav</h1>
-        </div>
-    )
-}
